686d2ecf345dcdb5b7523efbb529484429bb57f5
dinhvanlinh02 <92985229+dinhvanlinh02@users.noreply.github.com>
2021-11-10 18:04:35 +0700

    assigment 6

---
+++ bai4 assigmenr 6.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#define MAX
+ main(){
+	int a[MAX],n;
+	
+	if(ChanCuoi(a,n)==-1)
+		printf("\nMang khong co so chan");
+	else
+		printf("\nGia tri chan cuoi cung : %d",ChanCuoi(a,n));
+
+	printf("\nChuc cac ban hoc tot");
+	getch();
+}
+	
+	
+	
+	
+	
+	
+	}
+	
